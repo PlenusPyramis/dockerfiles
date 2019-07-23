11cdb56827ecc4726b8d4fc47e32ba1b6b8f2e16
Ryan McGuire <ryan@enigmacurry.com>
2019-07-22 22:30:14 -0400

    work on preseed

--- debian-pxe-provision/default_config/preseed_templates.py
+++ debian-pxe-provision/default_config/preseed_templates.py
@@ -3,51 +3,42 @@ from mako.template import Template
 from mako.lookup import TemplateLookup
 from mako import exceptions as mako_exceptions
 import logging
+from docker_configurator import load_merged_config, render_to_files, deep_merge
 
 logging.basicConfig(level=logging.INFO)
 logger=logging.getLogger(__name__)
 
-with open("/config/config.yaml") as f:
-    config = yaml.safe_load(f)
 
 template_lookup = TemplateLookup(directories=['/config/templates'])
 preseed_template = template_lookup.get_template("preseed.cfg.mako")
 pxelinux_template = template_lookup.get_template("pxelinux.cfg.mako")
 bootscreen_template = template_lookup.get_template("boot_screen.cfg.mako")
+postinstall_template = template_lookup.get_template("post_install.sh.mako")
 
-for mac, host_config in config['clients'].items():
-    host_config = {**config, **host_config}
-    preseed_path = "/tftp/preseed/{mac}.cfg".format(mac=mac)
-    preseed_alt_path = "/tftp/preseed/{mac}.cfg".format(mac=mac.replace("-",":"))
-    logging.info("Writing preseed configs: {} {}".format(preseed_path, preseed_alt_path))
-    try:
-        preseed = preseed_template.render(**host_config)
-    except:
-        print(mako_exceptions.text_error_template().render())
-        raise
+def main():
+    config = load_merged_config()
+    for mac, client_config in config['clients'].items():
+        preseed_path = "/tftp/preseed/{mac}.cfg".format(mac=mac)
+        preseed_alt_path = "/tftp/preseed/{mac}.cfg".format(mac=mac.replace("-",":"))
+        pxelinux_path = "/tftp/pxelinux.cfg/01-{mac}".format(mac=mac)
+        bootscreen_path = "/tftp/debian-installer/amd64/boot-screens/{mac}.cfg".format(mac=mac)
+        postinstall_path = "/tftp/preseed/{mac}_postinstall.sh".format(mac=mac)
 
-    with open(preseed_path, 'w') as f:
-        f.write(preseed)
+        client_merged_config = deep_merge(config['client_defaults'], client_config)
 
-    with open(preseed_alt_path, 'w') as f:
-        f.write(preseed)
+        render_to_files(preseed_template, [preseed_path, preseed_alt_path],
+                        mac=mac,
+                        dhcp=config['dhcp'],
+                        debian_mirror=config['debian_mirror'],
+                        public_ip=config['public_ip'], **client_merged_config)
+        render_to_files(pxelinux_template, pxelinux_path, mac=mac)
+        render_to_files(bootscreen_template, bootscreen_path,
+                        dhcp=config['dhcp'],
+                        public_ip=config['public_ip'],
+                        installer_priority=config['installer_priority'],
+                        mac=mac)
+        render_to_files(postinstall_template, postinstall_path,
+                        dns=config['dns'], **client_merged_config)
 
-    pxelinux_path = "/tftp/pxelinux.cfg/01-{mac}".format(mac=mac)
-    logging.info("Writing pxelinux config: {}".format(pxelinux_path))
-    try:
-        pxelinux = pxelinux_template.render(mac=mac)
-    except:
-        print(mako_exceptions.text_error_template().render())
-        raise
-    with open(pxelinux_path, 'w') as f:
-        f.write(pxelinux)
-
-    bootscreen_path = "/tftp/debian-installer/amd64/boot-screens/{mac}.cfg".format(mac=mac)
-    logging.info("Writing bootscreen config: {}".format(bootscreen_path))
-    try:
-        bootscreen = bootscreen_template.render(public_ip=config['public_ip'], mac=mac)
-    except:
-        print(mako_exceptions.text_error_template().render())
-        raise
-    with open(bootscreen_path, 'w') as f:
-        f.write(bootscreen)
+if __name__ == "__main__":
+    main()

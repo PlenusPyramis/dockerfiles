3387ddf90038e9b4a8fbbe79095ced35024ddab7
Ryan McGuire <ryan@enigmacurry.com>
2019-07-25 21:57:50 -0400

    coreos static network .. sorta

--- menu-pxe-provision/run.sh
+++ menu-pxe-provision/run.sh
@@ -11,5 +11,19 @@ echo "### Computed dnsmasq.conf:"
 cat -n /etc/dnsmasq.conf
 echo ""
 
+echo "### /data/ignition/default_ignition.yaml "
+cat /data/ignition/default_ignition.yaml
+echo ""
+
+echo "### /data/ignition/default_ignition.ign "
+cat /data/ignition/default_ignition.ign
+echo ""
+
+if [ $(cat /data/ignition/default_ignition.ign | wc -l) -eq 0 ]; then
+    echo "Ignition file is empty. Check for the fcct error above. Your config is probably bad."
+    exit 1
+fi
+
+
 #### Start the DHCP/tftp server:
 dnsmasq -C /etc/dnsmasq.conf --dhcp-hostsfile /etc/dhcp_hosts.txt --no-daemon

b585a1e6f6ac0e598c39e940e3c5e5685ea3494c
Ryan McGuire <ryan@enigmacurry.com>
2020-01-13 18:32:03 -0500

    Droplet dev script improvements

--- cloud-init/droplet.sh
+++ cloud-init/droplet.sh
@@ -6,41 +6,65 @@
 ## First step is configure doctl on your system and setup the API key
 ## Follow guide: https://github.com/digitalocean/doctl#installing-doctl
 
-## Second step configure your Float IP Address, Droplet name, and SSH KEY ID:
-## (floating ip has to be setup from a prior droplet beforehand):
+## Second step configure below: your Droplet name, region, floating IP address,
+## SSH key fingerprints, the Volumes to mount, and the User Data cloud-config
+## file (Volume must be created by hand beforehand. Floating IP address must be
+## setup from a prior droplet beforehand.)
 
 ## Third step, source this file in your bash session.
-## Then run functions to create and destroy the droplet.
+## Then run functions to create and destroy the droplet, view the logs, or run SSH.
 
+export DROPLET_REGION=nyc1
 export DROPLET_NAME=d
+export DROPLET_IMAGE=docker-18-04
+export DROPLET_SIZE=s-1vcpu-1gb
 export FLOATING_IP=167.172.12.217
-export SSH_KEY=26184694
+export SSH_FINGERPRINTS=76:ef:9f:d2:36:c9:c1:36:79:a5:8c:15:fb:bc:d8:64,e6:ac:de:dc:41:63:d6:56:b7:d2:ee:c3:56:b8:4e:47
+export VOLUME_NAMES=volume-nyc1-traefik-config
 export USER_DATA_FILE=traefik-photostructure.yaml
 
 droplet_destroy() {
-    DROPLET=$(doctl compute droplet list --format=ID,Name --no-header | grep " $DROPLET_NAME$" | cut -d " " -f 1)
-    if [ -z "$DROPLET" ]
-    then
-        echo Droplet does not exist: $DROPLET_NAME
-    else
-        doctl compute droplet delete $DROPLET --force
-        echo "Deleting droplet $DROPLET ..."
-        sleep 10
-    fi
+    (
+        set -e
+        DROPLET=$(doctl compute droplet list --format=ID,Name --no-header | grep " $DROPLET_NAME$" | cut -d " " -f 1)
+        if [ -z "$DROPLET" ]
+        then
+            echo Droplet does not exist: $DROPLET_NAME
+        else
+            doctl compute droplet delete $DROPLET --force
+            echo "Deleting droplet $DROPLET ..."
+            sleep 10
+        fi
+    )
+    return $?
 }
 
 droplet_create() {
-    DROPLET=$(doctl compute droplet list --format=ID,Name --no-header | grep " $DROPLET_NAME$" | cut -d " " -f 1)
-    if [ -z "$DROPLET" ]
-    then
-        echo Creating droplet $DROPLET_NAME ...
-        DROPLET=$(doctl compute droplet create $DROPLET_NAME --size s-1vcpu-1gb --image docker-18-04 --region nyc1 --ssh-keys $SSH_KEY --user-data-file $USER_DATA_FILE --wait --no-header --format=ID)
-        echo Assigning Floating IP to $DROPLET_NAME
-        doctl compute floating-ip-action assign $FLOATING_IP $DROPLET
-    else
-        echo Droplet already exists: $DROPLET_NAME ID=$DROPLET
-        return 1
-    fi
+    (
+        set -e
+        ## Get Volume IDS
+        VOLUMES=""
+        IFS=',' read -ra NAMES <<< "$VOLUME_NAMES"
+        for name in "${NAMES[@]}"; do
+            VOLUMES=$(doctl compute volume list --no-header | grep " $name " | cut -d ' ' -f 1),$VOLUMES
+        done
+        VOLUMES=$(echo $VOLUMES | sed 's/,*$//' | sed 's/^,*//')
+
+        ## Create droplet
+        DROPLET=$(doctl compute droplet list --format=ID,Name --no-header | grep " $DROPLET_NAME$" | cut -d " " -f 1)
+        if [ -z "$DROPLET" ]
+        then
+            echo Creating droplet $DROPLET_NAME ...
+            DROPLET=$(doctl compute droplet create $DROPLET_NAME --size $DROPLET_SIZE --image $DROPLET_IMAGE --region $DROPLET_REGION --volumes $VOLUMES --ssh-keys $SSH_FINGERPRINTS --user-data-file $USER_DATA_FILE --wait --no-header --format=ID)
+            test $? -eq 0 || exit 1
+            echo Assigning Floating IP to $DROPLET_NAME
+            doctl compute floating-ip-action assign $FLOATING_IP $DROPLET
+        else
+            echo Droplet already exists: $DROPLET_NAME ID=$DROPLET
+            return 1
+        fi
+    )
+    return $?
 }
 
 droplet_ssh() {
@@ -59,6 +83,7 @@ droplet_logs() {
 }
 
 droplet_recreate() {
+
     droplet_destroy && droplet_create && echo "Waiting 30s before trying to connect ..." && sleep 30 && \
         echo "Checking cloud-init status ... " && droplet_status
 }
